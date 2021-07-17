39063b4582057a855b784ccf604728a04c06e0f6
scarter31191 <scarter31191@gmaile.com>
2021-07-17 10:14:06 -0400

    staged nav links

--- portfolio/src/components/NavBar/NavBar.js
+++ portfolio/src/components/NavBar/NavBar.js
@@ -1,7 +1,22 @@
 const NavBar = () => {
     return (
         <div> 
-            NavBar 
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="/#skills">Skills</a>
+                    </li>
+                    <li>
+                        <a href="/#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
       );
 }
