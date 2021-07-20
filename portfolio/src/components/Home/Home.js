f08df1200a0e305c671cf558355b4d0a6ea10faa
scarter31191 <scarter31191@gmaile.com>
2021-07-19 22:49:10 -0400

    removed image

--- portfolio/src/components/Home/Home.js
+++ portfolio/src/components/Home/Home.js
@@ -1,7 +1,13 @@
+import classes from './Home.module.css'
+// import code from './../../img/background.jpg'
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className={classes.Home}>
+            <div className={classes.Container}>
+                <h1 className={classes.Hello}>Hello, World.</h1>
+                <h1>Welcome to my website </h1>
+            </div>
+            {/* <img className={classes.Code}  src={code} alt="code"></img> */}
         </div>
       );
 }
